cbf87a33ddda0d2284d5b8ed83de0d5d94766be0
User <User@DESKTOP-AEHS4BU>
2024-07-15 15:01:56 +0700

    Add export/import interface and exel manager

---
+++ src/CarNumberGuide/CarNumberGuideApp/Service/RegionsExporter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarNumberGuideApp.Entity;
+
+namespace CarNumberGuideApp.Service
+{
+    internal interface RegionsExporter
+    {
+        public void Export(List<Region> regions, string path);
+        public List<Region> Import(string path);
+    }
+}
